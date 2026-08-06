e60e36612c5f03c68c30a1e03bd782fffcb60d9a
jm8468 <jmtoyplay1113@gmail.com>
2026-08-07 00:06:43 +0900

    feat: 서류 평가 마감일 조회 API 연동

--- apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~document/~index.tsx
+++ apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~document/~index.tsx
@@ -1,4 +1,4 @@
-import { useSuspenseQueries } from '@tanstack/react-query';
+import { useSuspenseQueries, useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 import { Lottie } from '@toss/lottie';
 import { PageLayout } from '@yourssu-inhouse/exterior/layout';
@@ -12,8 +12,12 @@ import { MdPerson } from 'react-icons/md';
 import { SwitchCase } from 'react-simplikit';
 
 import { applicantByIdOption, applicantDocumentAnswersOption } from '@/apis/applicants/query';
-import { applicantDocumentCommentsOption } from '@/apis/documents/query';
+import {
+  applicantDocumentCommentsOption,
+  getPartDocumentsDeadlineOption,
+} from '@/apis/documents/query';
 import { meOption } from '@/apis/members/query';
+import { partsOption } from '@/apis/parts/query';
 import { Paper } from '@/components/Paper';
 import { EvalForm } from '@/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~document/components/EvalForm';
 import { FinalEvalDialog } from '@/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~document/components/FinalEvalDialog';
@@ -29,13 +33,18 @@ import { Answer } from './components/Answer';
 const RouteComponent = () => {
   const { applicantId } = Route.useParams();
 
-  const [{ data: applicant }, { data: answers }, { data: comments }] = useSuspenseQueries({
-    queries: [
-      applicantByIdOption(Number(applicantId)),
-      applicantDocumentAnswersOption(Number(applicantId)),
-      applicantDocumentCommentsOption(Number(applicantId)),
-    ],
-  });
+  const [{ data: applicant }, { data: answers }, { data: comments }, { data: parts }] =
+    useSuspenseQueries({
+      queries: [
+        applicantByIdOption(Number(applicantId)),
+        applicantDocumentAnswersOption(Number(applicantId)),
+        applicantDocumentCommentsOption(Number(applicantId)),
+        partsOption(),
+      ],
+    });
+
+  const part = parts.find((part) => part.partName === applicant.part) ?? parts[0];
+  const { data: deadline } = useSuspenseQuery(getPartDocumentsDeadlineOption(part.partId));
 
   const [sidebarView, setSidebarView] = useState<'문항 설정' | '서류 평가'>('서류 평가');
 
@@ -96,8 +105,9 @@ const RouteComponent = () => {
             <div className="text-sm">
               <span className="text-violet600 font-medium">서류 평가</span>
               <span className="text-neutralSubtle mx-1.5">·</span>
-              {/* TODO: GET /parts/{partId}/documents/deadline 생기면 연결하기 */}
-              <span className="text-neutralSubtle">3일 4시간 23분 남음</span>
+              <span className="text-neutralSubtle">
+                {formatTemplates['(2026년)? 1월 1일, 23:00'](deadline.deadline)} 마감
+              </span>
             </div>
             <div className="text-xl font-semibold">{applicant.name} 지원자</div>
           </div>
