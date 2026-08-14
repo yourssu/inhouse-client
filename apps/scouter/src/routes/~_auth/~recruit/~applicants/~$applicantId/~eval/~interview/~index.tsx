b9950d9525158362c82cbffab0c808d8fafe4dca
임서준 <84189784+jm8468@users.noreply.github.com>
2026-08-14 20:13:32 +0900

    [SCO-196] feat(scouter): 지원서 탭 구현 (#102)
    # 📝 변경 사항
    
    ## 개요
    
    면접 평가 페이지 InterviewTab의 `지원서` 탭 콘텐츠 구현.
    <img width="501" alt="image"
    src="https://github.com/user-attachments/assets/5bcd55a8-a01c-4349-b364-95736f2e0d31"
    />
    
    
    ### 접근성 적용
    https://namethatui.com/web/tabs 을 참고해 InterviewTab에 접근성을 적용했어요. 이 컴포넌트가
    일반적인 탭과는 달라서 똑같이 적용하지는 않았습니다.
    
    ### 지원자 서류 응답 조회 Query > StaleTime = Infinity
    지원자 서류 응답 조회 API는 항상 동일한 값이 들어올 것이라고 예상해서 staleTime을 Infinity로 뒀어요.
    현재는 컴포넌트에서만 staleTime을 오버라이드해뒀는데, 
    `query.ts`의 `applicantDocumentAnswersOption` 자체에 `staleTime: Infinity`를
    주는 건 어떻게 생각하시는지 궁금합니다!
    
    ## to-be
    
    
    <img width="2108" height="1214" alt="스크린샷 2026-08-13 225613"
    src="https://github.com/user-attachments/assets/a31434fb-d20a-4e2e-b692-936ec319f0ea"
    />
    
    
    # 🔗 같이 보면 좋아요
    
    ## Figma
    
    해당 없음
    
    ## Slack
    
    해당 없음

--- apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~interview/~index.tsx
+++ apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~interview/~index.tsx
@@ -4,9 +4,10 @@ import { PageLayout } from '@yourssu-inhouse/exterior/layout';
 import { Suspense } from 'react';
 import { SwitchCase } from 'react-simplikit';
 
-import { applicantByIdOption } from '@/apis/applicants/query';
+import { applicantByIdOption, applicantDocumentAnswersOption } from '@/apis/applicants/query';
 import { Paper } from '@/components/Paper';
 import { InterviewTab } from '@/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~interview/components/InterviewTab';
+import { DocumentAnswerForInterview } from '@/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~interview/components/InterviewTab/DocumentAnswerForInterview';
 import { ApplicantPageHeader } from '@/routes/~_auth/~recruit/~applicants/~$applicantId/components/ApplicantPageHeader';
 
 const INTERVIEW_TABS = ['질문', '지원서'] as const;
@@ -26,7 +27,7 @@ const RouteComponent = () => {
               <SwitchCase
                 caseBy={{
                   질문: () => <div>질문 콘텐츠 준비 중</div>,
-                  지원서: () => <div>지원서 콘텐츠 준비 중</div>,
+                  지원서: () => <DocumentAnswerForInterview applicantId={Number(applicantId)} />,
                 }}
                 value={tab}
               />
@@ -50,5 +51,6 @@ export const Route = createFileRoute('/_auth/recruit/applicants/$applicantId/eva
     const applicantId = Number(params.applicantId);
 
     context.queryClient.prefetchQuery(applicantByIdOption(applicantId));
+    context.queryClient.prefetchQuery(applicantDocumentAnswersOption(applicantId));
   },
 });
