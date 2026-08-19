7731c308d4a04ca309ec988a20c13b9ef06ea919
jm8468 <jmtoyplay1113@gmail.com>
2026-08-20 03:32:20 +0900

    refactor: query.ts에서 mutation 코드 삭제

--- apps/scouter/src/apis/interviews/rubrics/query.ts
+++ apps/scouter/src/apis/interviews/rubrics/query.ts
@@ -1,9 +1,9 @@
-import { mutationOptions, queryOptions } from '@tanstack/react-query';
+import { queryOptions } from '@tanstack/react-query';
 import { pluginQueryKey } from '@yourssu-inhouse/mfa-core';
 
 import type { InterviewRubricParams } from '@/apis/interviews/rubrics/schema';
 
-import { getInterviewRubric, updateInterviewRubric } from '@/apis/interviews/rubrics';
+import { getInterviewRubric } from '@/apis/interviews/rubrics';
 
 const qk = pluginQueryKey('scouter');
 
@@ -17,12 +17,3 @@ export const interviewRubricOption = (params: InterviewRubricParams) =>
     queryKey: interviewRubricQueryKeys.part(params),
     queryFn: () => getInterviewRubric(params),
   });
-
-export const updateInterviewRubricMutationOptions = mutationOptions({
-  mutationFn: updateInterviewRubric,
-  onSuccess: (_, { partId, semester }, _onMutateResult, context) =>
-    // 변경한 파트 루브릭 쿼리 무효화
-    context.client.invalidateQueries({
-      queryKey: interviewRubricQueryKeys.part({ partId, semester }),
-    }),
-});
