88d6353aa1226a7aa73f347dbd1cef882dfb1eb2
jm8468 <jmtoyplay1113@gmail.com>
2026-08-20 04:10:17 +0900

    feat: InterviewScoreInput에 disabled Props 추가

--- apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/components/InterviewScoreInput/index.tsx
+++ apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/components/InterviewScoreInput/index.tsx
@@ -2,6 +2,7 @@ import { cn } from '@yourssu-inhouse/interior-tailwind/utils';
 
 interface InterviewScoreInputProps {
   ariaLabel: string;
+  disabled?: boolean;
   invalid: boolean;
   /** 지정하면 이 값을 초과해 입력할 수 없고, 자동으로 이 값으로 잘려요. */
   maxScore?: number;
@@ -12,6 +13,7 @@ interface InterviewScoreInputProps {
 
 export const InterviewScoreInput = ({
   ariaLabel,
+  disabled,
   invalid,
   maxScore,
   onBlur,
@@ -23,8 +25,10 @@ export const InterviewScoreInput = ({
     aria-label={ariaLabel}
     className={cn(
       'h-lg rounded-8 border-grey200 focus:border-violet500 hover:not-focus:not-disabled:border-violetOpacity200 w-16 shrink-0 border px-3 text-right outline-none',
+      'disabled:bg-greyOpacity200 disabled:border-greyOpacity50 disabled:cursor-not-allowed',
       invalid && 'border-red600',
     )}
+    disabled={disabled}
     inputMode="numeric"
     onBlur={onBlur}
     onChange={(event) => {
