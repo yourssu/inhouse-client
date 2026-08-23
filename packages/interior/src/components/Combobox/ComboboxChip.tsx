393fa779b0f3b8673d4365515c1471cfa9c88853
2wndrhs <2wndrhs@gmail.com>
2026-08-23 11:07:11 +0900

    fix(interior): 비활성 Combobox 칩 삭제를 막는다

--- packages/interior/src/components/Combobox/ComboboxChip.tsx
+++ packages/interior/src/components/Combobox/ComboboxChip.tsx
@@ -3,16 +3,22 @@ import { MdClose } from 'react-icons/md';
 import * as styles from './Combobox.css';
 
 interface ComboboxChipProps<T extends string> {
+  disabled?: boolean;
   item: T;
   onRemove: (item: T) => void;
 }
 
-export const ComboboxChip = <T extends string>({ item, onRemove }: ComboboxChipProps<T>) => {
+export const ComboboxChip = <T extends string>({
+  disabled,
+  item,
+  onRemove,
+}: ComboboxChipProps<T>) => {
   return (
     <span className={styles.chip}>
       {item}
       <button
         className={styles.chipCloseButton}
+        disabled={disabled}
         onClick={(e) => {
           e.stopPropagation();
           onRemove(item);
