c815be770eaf80d57dd53a03345c1788f0a1b34e
임서준 <jmtoyplay1113@gmail.com>
2026-09-08 15:05:21 +0900

    refactor: onError 콜백에 대한 비동기 삭제

--- apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~interview/components/FinalInterviewEvaluationDialog.tsx
+++ apps/scouter/src/routes/~_auth/~recruit/~applicants/~$applicantId/~eval/~interview/components/FinalInterviewEvaluationDialog.tsx
@@ -43,8 +43,8 @@ export const FinalInterviewEvaluationDialog = ({
   const { isPending: isPassPending, mutateWithToast: passMutateWithToast } = useToastedMutation({
     errorText: '최종 면접 결과를 저장하지 못했어요.',
     mutationFn: () => patchApplicant({ applicantId, data: { state: 'FINAL_ACCEPTED' } }),
-    onError: async () => {
-      await invalidate();
+    onError: () => {
+      invalidate();
     },
     onSuccess: async () => {
       trackDecisionComplete('FINAL_ACCEPTED');
