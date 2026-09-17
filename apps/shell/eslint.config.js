b618cb3a3004c00fd1696623771cfa09a1d1ac8b
Sanghyeok Park <fecapark@gmail.com>
2026-09-18 00:08:21 +0900

    chore(shell): eslint ignore 목록에 @mf-types 추가 (#215)
    # 📝 변경 사항
    
    ## 개요
    
    <!-- 이미지 / 동영상 / 설명 -->
    
    module federation이 자동으로 생성하는 타입들을 eslint가 추적하지 않도록 수정했어요.
    
    ## as-is
    
    eslint가 이 폴더를 추적하고 있어서 shell `check:type` 에서 터지고 있었어요.
    
    ## to-be
    
    <!-- 이미지 / 동영상 / 설명 -->
    
    eslint가 추적할 필요가 없어서 제외시켜요.
    
    # 🔗 같이 보면 좋아요
    
    ## Figma
    
    <!-- 링크 -->
    
    ## Slack
    
    <!-- 링크 -->

--- apps/shell/eslint.config.js
+++ apps/shell/eslint.config.js
@@ -3,7 +3,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['**/*.gen.*', 'public/**']),
+  globalIgnores(['**/*.gen.*', 'public/**', '@mf-types/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [workspaceReactEslintConfig, reactRefresh.configs.vite],
